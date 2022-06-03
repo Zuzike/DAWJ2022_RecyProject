e4bb748ee850eec3232b462fad7f35cb96d4b76a
Ivana Plašienková <ivana.plasienkova@gmail.com>
2022-06-03 20:25:22 +0200

    Komponenta Rules

---
+++ src/Rules/index.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import './style.css';
+
+export const Icon = (props) => {
+  return (
+    <>
+      <div className="rules">
+        <h1 className="rules_title">Päť pravidiel recyklácie</h1>
+        <div className="icons">
+          <div className="icon icon_refuse">REFUSE (Odmietam)</div>
+          <p className="icons_describe">
+            Ak si naozaj potrebujem niečo nové kúpiť, zamyslím sa nad obalom,
+            pôvodom, kvalitou, férovosťou a spôsobom výroby a spracovania.
+            Odmietam obaly a výrobky, ktoré vlastne nepotrebujem alebo neviem
+            znovu využiť, vytriediť či skompostovať. Odmietam letáky a reklamné
+            predmety zadarmo.
+          </p>
+        </div>
+        <div />
+        <div className="icons">
+          <div className="icon icon_reduce">REDUCE (Redukujem)</div>
+          <p className="icons_describe">
+            Redukujem množstvo vecí, ktoré nakupujem, či používam. Vyhýbam sa
+            impulzívnemu nakupovaniu a nakupovaniu samoúčelných vecí. S každou
+            novou vecou vzniká ďalší odpad. Každý nákup si teda poriadne
+            premyslím.
+          </p>
+        </div>
+        <div className="icons">
+          <div className="icon icon_reuse">REUSE (Znovu využijem)</div>
+          <p className="icons_describe">
+            Veci používam opakovane. Pokiaľ vec už doslúžila svojmu primárnemu
+            účelu, zamyslím sa nad inou možnosťou jej využitia. Zároveň myslím
+            na to, že každá jednorázová vec má svoju alternatívu, ktorá je
+            vhodná na viac použití. Aj týmto obmedzím vytváranie odpadu.
+          </p>
+        </div>
+        <div className="icons">
+          <div className="icon icon_recycle">RECYCLE (Recyclujem)</div>
+          <p className="icons_describe">
+            Keď už vec naozaj doslúžila, snažím sa ju správne vytriediť, aby
+            mohla byť zrecyklovaná. Na to, aby jeho recyklácia bola účelná,
+            myslím už pri kúpe výrobku. Zmesovému odpadu sa chceme vyhnúť,
+            nakoľko ten končí na skládke odpadov alebo v spaľovni s energetickým
+            zhodnotením.
+          </p>
+        </div>
+        <div className="icons">
+          <div className="icon icon_rot">ROT (Kompostujem)</div>
+          <p className="icons_describe">
+            Kompostovanie ako riadený proces rozkladu biologicky rozložiteľného
+            odpadu je najlepším spôsobom využitia predovšetkým rastlinného
+            odpadu z kuchyne a záhrady. Pri kompostovaní sa odpad stáva potravou
+            pre iné organizmy, ktoré z neho vytvoria kvalitné hnojivo aj na
+            pestovanie našej potravy.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
--- src/index.jsx
+++ src/index.jsx
@@ -5,22 +5,29 @@ import { Parallax, Background } from 'react-parallax';
 import img1 from './img/buckets.jpg';
 import img2 from './img/bird_trash.jpg';
 import img3 from './img/water_pipe.jpg';
+import { Icon } from './Rules';
 
 const App = () => (
   <div className="main">
     <Parallax bgImage={img1} strength={300}>
       <div style={{ height: 800 }}>
         <div className="strip strip__one">
-          <h1 className="strip__title">Návod </h1>
+          <h1 className="strip__title">
+            {' '}
+            V našej krajine máme rôzne možnosti triedenia odpadov. Vyber si z
+            ponuky smetných košov tie, ktoré vídaš vo svojom okolí. Potom do
+            vyhľadávača napíš, čoho presne sa potrebuješ zbaviť. Vyhľadávač sa
+            posnaží ťa nasmerovať na správnu odpadovú nádobu.
+          </h1>
         </div>
       </div>
     </Parallax>
     <h2>| | |</h2>
     <Parallax bgImage={img2} strength={300}>
-      <div style={{ height: 600 }}>
+      <div style={{ height: 500 }}>
         <div className="strip strip__two">
           {' '}
-          <h1 className="strip__title">Päť pravidiel recyklácie</h1>
+          <Icon />
         </div>
       </div>
     </Parallax>
