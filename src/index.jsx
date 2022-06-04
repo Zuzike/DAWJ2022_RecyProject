17945273045c9ac064a2be3a073625c1021ba570
Zuzike Nemesová <darigjorkinen@gmail.com>
2022-06-04 15:59:00 +0200

    Interaktivita Variations hotová

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles.css';
-import { Parallax, Background } from 'react-parallax';
+import { Parallax } from 'react-parallax';
 import img1 from './img/buckets.jpg';
 import img2 from './img/bird_trash.jpg';
 import img3 from './img/water_pipe.jpg';
@@ -9,47 +9,50 @@ import { Icon } from './Rules';
 import { Search } from './Search';
 import { Variations } from './Variations';
 
-const App = () => (
-  <div className="main">
-    <Parallax bgImage={img1} strength={300}>
-      <div style={{ height: 800 }}>
-        <div className="strip strip__one">
-          <h1 className="strip__title">
+const App = () => {
+  const [group, setGroup] = useState('');
+  return (
+    <div className="main">
+      <Parallax bgImage={img1} strength={300}>
+        <div style={{ height: 800 }}>
+          <div className="strip strip__one">
+            <h1 className="strip__title">
+              {' '}
+              V našej krajine máme rôzne možnosti triedenia odpadov. Vyber si z
+              ponuky smetných košov tie, ktoré vídaš vo svojom okolí. Potom do
+              vyhľadávača napíš, čoho presne sa potrebuješ zbaviť. Vyhľadávač sa
+              posnaží ťa nasmerovať na správnu odpadovú nádobu.
+            </h1>
+          </div>
+        </div>
+      </Parallax>
+      <div className="search">
+        <Variations group={group} onChangeGroup={setGroup} />
+        <Search />
+      </div>
+      <Parallax bgImage={img2} strength={300}>
+        <div style={{ height: 500 }}>
+          <div className="strip strip__two">
             {' '}
-            V našej krajine máme rôzne možnosti triedenia odpadov. Vyber si z
-            ponuky smetných košov tie, ktoré vídaš vo svojom okolí. Potom do
-            vyhľadávača napíš, čoho presne sa potrebuješ zbaviť. Vyhľadávač sa
-            posnaží ťa nasmerovať na správnu odpadovú nádobu.
-          </h1>
+            <Icon />
+          </div>
         </div>
-      </div>
-    </Parallax>
-    <div className="search">
-      <Variations />
-      <Search />
-    </div>
-    <Parallax bgImage={img2} strength={300}>
-      <div style={{ height: 500 }}>
-        <div className="strip strip__two">
-          {' '}
-          <Icon />
-        </div>
-      </div>
-    </Parallax>
+      </Parallax>
 
-    <h2>| | |</h2>
-    <Parallax bgImage={img3} strength={-100}>
-      <div style={{ height: 700 }}>
-        <div className="strip strip__three">
-          <h1 className="strip__title">Niečo zmysluplné</h1>
+      <h2>| | |</h2>
+      <Parallax bgImage={img3} strength={-100}>
+        <div style={{ height: 700 }}>
+          <div className="strip strip__three">
+            <h1 className="strip__title">Niečo zmysluplné</h1>
+          </div>
         </div>
-      </div>
-    </Parallax>
-    <h2>| | |</h2>
-    <Parallax strength={500}></Parallax>
-    <div style={{ height: 500 }} />
-    <h2>{'\u2728'}</h2>
-  </div>
-);
+      </Parallax>
+      <h2>| | |</h2>
+      <Parallax strength={500}></Parallax>
+      <div style={{ height: 500 }} />
+      <h2>{'\u2728'}</h2>
+    </div>
+  );
+};
 
 createRoot(document.querySelector('#app')).render(<App />);
