2a8c38781ba69261ddf95860301f7c767fa68b05
Ivana Plašienková <ivana.plasienkova@gmail.com>
2022-06-04 16:47:49 +0200

    Pridaná komponenta Contajners

---
+++ src/Containers/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './style.css';
+
+export const Containers = () => {
+  return (
+    <div className="containers">
+      <div className="container">PAPIER</div>
+      <div className="container">SKLO</div>
+      <div className="container">PLASTY</div>
+      <div className="container">KOVY</div>
+      <div className="container">NÁOPOJOVÉ KARTÓNY</div>
+      <div className="container">ZÁHRADNÝ ODPAD</div>
+      <div className="container">KUCHUNSKÝ ODPAD</div>
+      <div className="container">JEDLÉ OLEJE A TUKY</div>
+      <div className="container">KOMPOST</div>
+      <div className="container">TEXTIL</div>
+      <div className="container">ZMIEŠANÝ ODPAD</div>
+    </div>
+  );
+};
