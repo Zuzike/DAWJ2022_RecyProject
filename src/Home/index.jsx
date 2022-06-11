df3b111c434ebba06b75104dab7ed32c93cb3031
Ivana Plašienková <ivana.plasienkova@gmail.com>
2022-06-11 19:28:37 +0200

    Zmena úvodného textu.

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -6,14 +6,12 @@ export const Home = () => {
     <div className="uvod">
       <h1 className="strip__title"> Vyhľadávač odpadových nádob </h1>
       <p className="strip__content">
-        Naučme sa správne vytriediť odpad, ktorý po nás denne ostáva. Často sa
-        nám stáva, že si nie sme istí, do akého kontajneru hodiť vec, ktorá nám
-        už doslúžila. Hoci na Slovensku máme systémy triedenia odpadov pestré ba
-        až zložité, triediť sa dá všade. Môže nám v tom pomôcť aj nasledujúci
-        vyhľadávač. Vyberte si z ponuky farebných kontajnerov tie, ktoré máte vo
-        svojom okolí k dispozícií a do okienka vyhľadávača zadajte názov veci,
-        ktorej sa chcete zbaviť. Vyhľadávač sa bude snažiť vybrať pre vás vhodný
-        kontajner.
+        Možno ste sa aj vy neraz ocitli v situácii, kedy ste vec ktorá vám už
+        doslúžila, chceli vytriediť a poslať tak na recykláciu. Tento dobrý
+        zámer sa však často nepodarí uskutočniť, pretože si nie sme istí do
+        ktorého kontajnera danú vec vyhodiť. Prinášame preto praktického
+        pomocníka v podobe online vyhľadávača, vďaka ktorému môžete kedykoľvek
+        rýchlo zisťiť kam s odpadom, aby vaše úsilie malo zmysel.
       </p>
     </div>
   );
