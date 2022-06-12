30e408eab380aac6530b24fd8cae084c8f26d3b2
Zuzike Nemesová <darigjorkinen@gmail.com>
2022-06-12 11:26:52 +0200

    uprava foto a webpack build

--- src/Footer/index.jsx
+++ src/Footer/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles.css';
-import czechitas from './img/czechitas_logo.png';
+import czechitas from './img/Czechitas_logo.png';
 
 export const Footer = () => {
   return (
